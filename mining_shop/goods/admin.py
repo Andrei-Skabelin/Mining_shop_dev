00edddae6445f3cf34403e0036b5eaff9997323b
Aliaksandr Hreben <greba3000@gmail.com>
2022-09-18 21:15:06 +0300

    Add product model to admin panel

--- mining_shop/goods/admin.py
+++ mining_shop/goods/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from goods.models import Product
 
-# Register your models here.
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name']
+
+
+admin.site.register(Product, ProductAdmin)
